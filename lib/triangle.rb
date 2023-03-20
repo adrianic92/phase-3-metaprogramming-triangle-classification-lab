0451045d317627f768b8f4cc6e0c93a4ead97473
Adrian <adrianic92@yahoo.com>
2023-03-20 11:28:50 -0700

    commit tree e19847ac5119f22a9b2f06390e7ef92913bf889c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+
+  attr_accessor :one, :two, :three
+
+  def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+  end
+
+  def kind
+
+    if one <= 0 || two <= 0 || three <= 0 || one + two <= three || one + three <= two || two + three <= one
+      raise TriangleError
+    end
+
+    if one == two && two == three
+      return :equilateral
+    elsif one == two || two == three || one == three
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    "Sorry, not a triangle..."
+  end
+
 end
